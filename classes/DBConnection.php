80beb6a117d40d2f67b3664067a6f5780efc678d
Andrea Valenza <avalenza89@gmail.com>
2013-11-21 23:27:58 +0100

    Pending v2.0

---
+++ classes/DBConnection.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Used by Database class.
+ * @todo Update it with your credentials.
+ */
+define( "DB_HOST", "localhost" );
+define( "DB_USER", "apnea" );
+define( "DB_PASS", "apnea" );
+define( "DB_NAME", "apnea" );
+
+define( "DB_TYPE", "mysql" );
---
+++ doc/elementlist.js
@@ -0,0 +1,3 @@
+
+var ApiGen = ApiGen || {};
+ApiGen.elements = [["c","DB\\Database"],["c","DB\\Row"],["c","DB\\Table"]];
